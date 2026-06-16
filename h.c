77a17a4a42c0d5ec9e1c5310a6b7815c3af6e404
LeangHuot_Chheav <leanghuot445566@gmail.com>
2026-06-16 10:15:47 +0700

    do ey kr ban thom hz

---
+++ h.c
@@ -0,0 +1,18 @@
+    int accountNumer = 1001;
+
+/* Function declarations */
+void mainMenu();
+void createAccount();
+int loginAccount();
+void accountMenu(int accountIndex);
+
+void depositCurrent(int accountIndex);
+void withdrawCurrent(int accountIndex);
+void transferMoney(int accountIndex);
+void exchangeCurrentMoney(int accountIndex);
+
+void moveCurrentToSaving(int accountIndex);
+void moveSavingToCurrent(int accountIndex);
+void addSavingInterest(int accountIndex);
+
+void checkBalance(int accountIndex);
