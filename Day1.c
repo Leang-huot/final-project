d9ff127ea71cd03c71e8c8aa84bf396327a513f0
LeangHuot_Chheav <leanghuot445566@gmail.com>
2026-06-23 00:55:56 +0700

    InterFace and Account Creation funtion

--- Day1.c
+++ Day1.c
@@ -1,59 +1,114 @@
 #include <stdio.h>
 #include <string.h>
-#define Limit_User 100
+#include <ctype.h>
 //Day 1 
+//Structure
 typedef struct {
+    char Acc_name[50];
     char username[50];
-    int password;
+    char password[20];
 }User_login;
 typedef struct{
-
-    char Acc_name[50];
+    int accountNumber;
     double USD;
     double KHR;
 
     double savingUSD;
     double savingKHR;
 
-    User_login;
+    User_login login;
 }account;
+//Global Variable
+#define Limit_User 100
+account Accounts[Limit_User];
+int accountNumber;
+int accountcount = 0;
+int NextAccountNumber = 100;
 
+void interface();
+void AccountCreation();
 
-AccountCreation();
-AccountLogin;
+int main(){
+    interface();
 
-
+    return 0;
+}
 void interface(){
-    int choice;
+    int choice = 0;
     
-    printf("----------WELCOME TO OUR BANK----------\n");
-
-    printf("1.Create account\n");
-    printf("Login into account\n");
-    printf("3.Log out from account\n");
-    printf("---------------------------------------\n");
-    printf("Enter your choice :");
-    scanf("%d", &choice);
 
     do{
+    printf("\n----------WELCOME TO OUR BANK----------\n");
+    printf("1.Create account\n");
+    printf("2.Login into account\n");
+    printf("3.Exit the app\n");
+    printf("---------------------------------------\n");
+   
+    printf("Enter your choice :");
+        scanf("%d", &choice);
         switch(choice){
             case 1: 
                 AccountCreation();
 
                 break;
             case 2:
-                AccountLogin();
+                printf("not create yet");
                 break;
             case 3: 
-                printf("Thanks You For using our service\n");
+                printf("----------THANK YOU FORR USING OUR APP----------\n");
                 break;
-                printf("Choice is Invalid,Please try again");
+        default :
+            printf("Invalid choice. Try again\n");
         }
     }while ( choice != 3);
 
 }
-int main(){
-    interface();
-    leanghuot;
-    return 0;
+
+void AccountCreation(){
+    int valid;
+    account NewAccount;
+    NewAccount.accountNumber = NextAccountNumber;
+    NextAccountNumber++;
+    if(accountcount >= Limit_User){
+        printf("Full user access. You cannot create account anymore\n");
+        return;
+    }
+    printf("------Please Create Your Account To Login------\n");
+    printf("Enter Account name : ");
+        scanf("%49s", NewAccount.login.Acc_name);
+    printf("Enter your username :");
+        scanf("%49s", NewAccount.login.username);
+do {
+    valid = 1;
+
+    printf("Enter Your Password: ");
+    scanf("%19s", NewAccount.login.password);
+
+    if (strlen(NewAccount.login.password) != 6) {
+        valid = 0;
+    }
+
+    for (int i = 0; i < strlen(NewAccount.login.password); i++) {
+        if (!isdigit(NewAccount.login.password[i])) {
+            valid = 0;
+        }
+    }
+
+    if (valid == 0) {
+        printf("Password must enter 6 number only.Please try again.\n");
+    }
+
+}while (valid == 0);
+    NewAccount.USD = 0;
+    NewAccount.KHR = 0;
+    NewAccount.savingUSD = 0;
+    NewAccount.savingKHR = 0;
+
+    Accounts[accountcount] = NewAccount;
+    accountcount++;
+    printf("\n------Account created successfully------!\n");
+    printf("Account Username: %s\n", NewAccount.login.Acc_name);
+    printf("Accont logint name : %s\n", NewAccount.login.username);
+    printf("Account ID: %d\n", NewAccount.accountNumber);
+    
 }
